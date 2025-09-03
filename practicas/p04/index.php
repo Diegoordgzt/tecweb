0659ddbc77635a537f320c0235c8a1fd04dc221d
DiegoRdgzz <rt202354027@alm.buap.mx>
2025-09-03 08:20:01 -0600

    ejercicio2: valores de , , /c/Users/diego/bash_completion.d/*.bash completado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -30,7 +30,43 @@
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
     ?>
-    
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:<br />
+    $a = “ManejadorSQL”;<br/>
+    $b = 'MySQL’;<br/>
+    $c = &amp;$a;</p>
+    <p>a. Ahora muestra el contenido de cada variable</p>
+<p>b. Agrega al código actual las siguientes asignaciones:<br/>
+    $a = “PHP server”;<br/>
+    $b = &amp;$a;</p>
+    <p>c. Vuelve a mostrar el contenido de cada uno</p>
+    <p>d. Describe en y muestra en la página obtenida qué ocurrió en el segundo bloque de
+    asignaciones</p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        // Asignaciones iniciales
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+        echo '<h4>Respuesta inciso a:</h4>';
+        echo "<p>Valores iniciales:</p>";
+        echo "a: $a <br />";
+        echo "b: $b <br />";
+        echo "c: $c <br />";
+        echo '<h4>Cambio hecho del inciso b.</h4>'; 
+        $a = "PHP server";
+        $b = &$a;
+        echo '<h4>Respuesta inciso c.</h4>';
+        echo "<p>Valores después del cambio:</p>";
+        echo "a: $a <br />";
+        echo "b: $b <br />";
+        echo "c: $c <br />";
+        echo '<h4>Respuesta inciso d.</h4>';
+        echo "<p>Explicación:</p>";
+        echo "La variable \$c es una referencia a \$a, por lo que al cambiar \$a, también cambia \$c. \n";
+        echo "Luego, \$b también se hace referencia a \$a, por lo que todas apuntan al mismo valor.";
+        unset($a, $b, $c);
+    ?>
 
 </body>
 </html>
